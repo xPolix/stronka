2c57c16e0859c04c451590c2c84c4b9be07c4c32
Janek <74066801+xPolix@users.noreply.github.com>
2020-11-24 23:17:00 +0100

    Update bindy_login_script.js

--- bindy_login_script.js
+++ bindy_login_script.js
@@ -2,7 +2,7 @@ function checkPswd() {
         var confirmPassword = "admin";
         var confirmKod = "16.";
         var password = document.getElementById("pswd").value;
-        var kod = document.getElementById("pswd").value;
+        var kod = document.getElementById("kodzik").value;
         if (password == confirmPassword && kod == confirmKod) {
              window.location="52.2391541,"+ kod +"8626765";
         }
